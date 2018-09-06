9fa4ecaeecaaeffc8b75e78ab742f3afe9bab12d
Ali <41242262+LazenLang@users.noreply.github.com>
2018-09-06 23:08:00 +0200

    Add files via upload

--- home/start_modules.jl
+++ home/start_modules.jl
@@ -1,20 +1,29 @@
 module start_modules
     include("../tokenizing/token.jl")
+    include("../tokenizing/tok_preprocess.jl")
     include("../analysis/tok_analysis.jl")
     include("../analysis/ast_analysis.jl")
     include("../text_util.jl")
     include("../parsing/ast_build.jl")
 
     function go(code)
-        tokens = token.go(code) # tokens[1] = modified token list | tokens[2] = raw token list
+        t0 = time_ns()
+        println("Compilation started...")
+
+        tokens = token.go(code)
         if !(tok_analysis.go(tokens[1], -1, tokens[2])); return; end
 
-        for (index, x) in enumerate(tokens[1])
+        for (index, x) in enumerate(tokens[3])
             println("tok: ", x)
             build_ast = ast_build.go(x)
             println("ln ", index, "\n", build_ast)
             println("---------")
         end
 
+        t1 = time_ns()
+        println("Compilation ended. Time taken in second is ", (t1 - t0) / 1000000000)
+        println("parsed")
+
+        println("finished")
     end
 end
